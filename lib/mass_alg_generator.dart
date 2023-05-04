fc5e4c62ecd4c65b2e5496ce80fc292d383ef752
markdid <mark@larsen.org>
2023-05-04 13:40:28 -0600

    fix main

--- lib/mass_alg_generator.dart
+++ lib/mass_alg_generator.dart
@@ -1,7 +1,56 @@
-library mass_alg_generator;
+import 'dart:io';
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:mass_alg_generator/generator/code_generator.dart';
+import 'package:mass_alg_generator/generator/variable_parser.dart';
+
+void main() async {
+  final variables = await parseVariablesFromFile('variables.txt');
+  final functions = _generateApiFunctions(variables);
+
+  final apiC = generateApiC(functions);
+  await File('api.c').writeAsString(apiC);
+
+  final dartFfiBridge = generateDartFfiBridgeCode(variables);
+  await File('ffi_bridge.dart').writeAsString(dartFfiBridge);
+}
+
+List<ApiFunction> _generateApiFunctions(List<Variable> variables) {
+  final List<ApiFunction> functions = [];
+
+  // Add non-getter/setter functions
+  functions.add(ApiFunction(
+    name: 'mass_alg_init',
+    returnType: 'void',
+  ));
+
+  functions.add(ApiFunction(
+    name: 'mass_alg_step',
+    returnType: 'void',
+  ));
+
+  functions.add(ApiFunction(
+    name: 'mass_alg_accel_step',
+    returnType: 'void',
+    parameters: ['float ax', 'float ay', 'float az'],
+  ));
+
+  // Add getter and setter functions for each variable
+  for (final variable in variables) {
+    functions.add(ApiFunction(
+      name: 'get_${variable.name.toLowerCase()}',
+      returnType: variable.type,
+      variableName: variable.name,
+      isGetter: true,
+    ));
+
+    functions.add(ApiFunction(
+      name: 'set_${variable.name.toLowerCase()}',
+      returnType: 'void',
+      variableName: variable.name,
+      isSetter: true,
+      parameters: [variable.type],
+    ));
+  }
+
+  return functions;
 }
